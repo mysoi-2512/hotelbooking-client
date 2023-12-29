268366490193e41e758f8dd90b35e14c7b91f98f
mysoi-2512 <my.vo.2512@gmail.com>
2023-12-29 13:12:17 +0700

    change url

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -1,7 +1,7 @@
 import axios from'axios';
 
 export const api = axios.create({
-    baseURL : "http://localhost:9192"
+    baseURL : "https://hotelbooking-server-production.up.railway.app/"
 })
 
 export const getHeader = () => {
